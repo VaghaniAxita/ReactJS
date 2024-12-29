d59754f66fee7c08c13c7da3e79d48997a61e709
VaghaniAxita <vaghaniaxita23@gmail.com>
2024-12-29 14:10:39 +0530

    app.jsx

--- Props/src/App.jsx
+++ Props/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
 
        <UserProfileCard
         name="ReactJS"
-        age={21}
+        age={2024}
         bio="React is a free, open-source JavaScript library"
         location="Surat, Gujarat"
         profilePicture="https://media.licdn.com/dms/image/C4E12AQEBVCR2SpRr9g/article-cover_image-shrink_720_1280/0/1625909824541?e=2147483647&v=beta&t=Y_zSoI8cPUR3wQvPyYK15PLWpQJJ0si6OvsuXFnIC18"
