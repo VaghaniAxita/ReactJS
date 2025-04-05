99d244db837881c9e588f41565a1482511786c89
VaghaniAxita <vaghaniaxita23@gmail.com>
2025-04-05 12:41:37 +0530

    login

--- Firebase_Auth/src/components/Login.jsx
+++ Firebase_Auth/src/components/Login.jsx
@@ -20,7 +20,7 @@ const SignUp = () => {
       <h2>Login</h2>
       <input type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
       <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-      <button onClick={handleSignUp}>Sign Up</button>
+      <button onClick={handleSignUp}>Login</button>
     </div>
   );
 };
